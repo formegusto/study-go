12f9277359008926a74669391df97cb1910a5457
formegusto <formegusto@gmail.com>
2022-12-30 12:40:03 +0900

    crawled data in csv

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"strconv"
 	"strings"
 
@@ -19,7 +21,7 @@ type extractedJob struct {
 }
 
 var baseURL string = "https://www.saramin.co.kr/zf_user/search/recruit?searchword=%ED%8C%8C%EC%9D%B4%EC%8D%AC&go=&flag=n&searchMode=1&searchType=search&search_done=y&search_optional_item=n&recruitSort=relation&recruitPageCount=40&inner_com_type=&company_cd=0%2C1%2C2%2C3%2C4%2C5%2C6%2C7%2C9%2C10&show_applied=&quick_apply=&except_read=&ai_head_hunting="
-var detailURL string = "https://www.saramin.co.kr/zf_user/jobs/relay/view?isMypage=no&recommend_ids=eJxNkMkRA0EIA6PxH8QheDsQ55%2BFZ7xrZp9dalGAA8gIfgr64ts9Fd35Kc2Nkg31hegrtXLWP3Wz1tx4dQkmOTLNV3kwrIMPWYqFByKrz%2BTOUg5GLDkGfa0lGEzz%2BKV6y2Lco%2BTeKoNHNpg5j1wUObIL102TZqBif0O%2Bi%2FpAcw%3D%3D&view_type=search&searchword=%ED%8C%8C%EC%9D%B4%EC%8D%AC&searchType=search&gz=1&t_ref_content=generic&t_ref=search&paid_fl=n&search_uuid=9390d19c-8703-43f7-8ba6-47b1d36862e3#seq=0"
+var detailURL string = "https://www.saramin.co.kr/zf_user/jobs/relay/view?isMypage=no&recommend_ids=eJxNkMkRA0EIA6PxH8QheDsQ55%2BFZ7xrZp9dalGAA8gIfgr64ts9Fd35Kc2Nkg31hegrtXLWP3Wz1tx4dQkmOTLNV3kwrIMPWYqFByKrz%2BTOUg5GLDkGfa0lGEzz%2BKV6y2Lco%2BTeKoNHNpg5j1wUObIL102TZqBif0O%2Bi%2FpAcw%3D%3D&view_type=search&searchword=%ED%8C%8C%EC%9D%B4%EC%8D%AC&searchType=search&gz=1&t_ref_content=generic&t_ref=search&paid_fl=n&search_uuid=9390d19c-8703-43f7-8ba6-47b1d36862e3"
 // &rec_idx=44621811
 
 func main() {
@@ -35,7 +37,29 @@ func main() {
 		jobs = append(jobs, extractedJobs...)
 	}
 
-	fmt.Println(jobs)
+	// fmt.Println(jobs)
+	writingJob(jobs)
+}
+
+func writingJob(jobs []extractedJob) {
+	file, err := os.Create("data/jobs.csv")
+	checkErr(err)
+
+	w := csv.NewWriter(file)
+	// 함수가 끝나는 시점에 CSV 파일에 데이터를 추가하는 함수
+	defer w.Flush()
+
+	headers := []string{"ID", "TITLE", "LOCATION", "SALARY", "STACK"}
+	wErr := w.Write(headers)
+	checkErr(wErr)
+
+	for _, job := range(jobs) {
+		jobSlice := []string{job.id, job.title, job.location, job.salary, job.stack}
+		jwErr := w.Write(jobSlice)
+		checkErr(jwErr)
+	}
+
+	fmt.Println("Done, extracted", len(jobs), )
 }
 
 func getPage(page int) []extractedJob {
@@ -67,7 +91,7 @@ func extractJob(card *goquery.Selection) extractedJob {
 	salary := cleanString(card.Find("div.job_condition span:nth-child(5)").Text())
 	stack := cleanString(card.Find("div.job_sector").Text())
 	return extractedJob{
-		id: id, 
+		id: detailURL + "&rec_idx=" + id + "#seq=0", 
 		title: title, 
 		location: location, 
 		salary: salary, 
